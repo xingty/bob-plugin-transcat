ffb0c49b0e76405f0504251d59beab904b405f3b
bigbyto <bigbyto@Bigbytos-iMac.local>
2023-05-31 15:13:41 +0800

    fix(release): sha256 is empty

--- release.sh
+++ release.sh
@@ -18,6 +18,19 @@ if [[ -z  desc ]]; then
   desc="Release v$version"
 fi
 
+vln=$(cat src/info.json | grep -n '"version"' | awk -F: 'NR==1{print $1}')
+if [[ $vln -gt 0 ]]; then
+  iv=$(cat src/info.json | grep -n '"version"' | awk -F\" 'NR==1{print $4}')
+  if [ $version != $iv ]; then
+    sed -i "${vln}s/.*/  \"version\": \"$version\",/" src/info.json
+    echo "Updating the version of the info.json from => $iv to => $version"
+  fi
+fi
+
+cv=$(cat appcast.json | grep '"version"' | awk -F\" 'NR==1 {print $4}')
+echo "Current version: $cv, build version: $version"
+/bin/bash ./build.sh $version
+
 ln=$(cat appcast.json | grep -n "\"$version\""|awk -F: 'NR==1{print $1}')
 sha256=$(sha256sum ./build/bob-plugin-transcat-v$version.bobplugin | awk '{print $1}')
 echo "sha256: $sha256"
@@ -32,16 +45,3 @@ else
   content="\    {\n     \"version\": \"$version\",\n      \"desc\": \"$desc\",\n      \"sha256\": \"$sha256\",\n      \"url\": \"$url\",\n      \"minBobVersion\": \"0.5.0\"\n    },"
   sed -i "${ln}i$content" appcast.json
 fi
-
-vln=$(cat src/info.json | grep -n '"version"' | awk -F: 'NR==1{print $1}')
-if [[ $vln -gt 0 ]]; then
-  iv=$(cat src/info.json | grep -n '"version"' | awk -F\" 'NR==1{print $4}')
-  if [ $version != $iv ]; then
-    sed -i "${vln}s/.*/  \"version\": \"$version\",/" src/info.json
-    echo "Updating the version of the info.json from => $iv to => $version"
-  fi
-fi
-
-cv=$(cat appcast.json | grep '"version"' | awk -F\" 'NR==1 {print $4}')
-echo "Current version: $cv, build version: $version"
-/bin/bash ./build.sh $version
